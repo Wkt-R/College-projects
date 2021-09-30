14e4abc78e0dbda6f01198bc2b3764a915bf5c27
Wkt-R <69642445+Wkt-R@users.noreply.github.com>
2021-09-30 13:53:34 +0200

    Add files via upload

---
+++ SklepKomputerowy/src/main/java/org/example/Const.java
@@ -0,0 +1,10 @@
+package org.example;
+
+// w tej klasie deklarujemy stałe dla wygodnego dostępu atrybutów w bazie
+public class Const {
+    public static final String USERS_TABLE = "users";
+    public static final String USERS_FIRSTNAME = "firstname";
+    public static final String USERS_SURNAME = "surname";
+    public static final String USERS_USERNAME = "username";
+    public static final String USERS_PASSWORD = "password";
+}
